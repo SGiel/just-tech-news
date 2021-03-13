6bad2473090dab05af2b9e80e0a460e0eb0905a6
Stephanie Giel <stephaniegiel@gmail.com>
2021-03-13 14:10:18 -0700

    created a homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+// Because we've hooked up a template engine, we can now useres.render() and specify which template we want to use. 
+// In this case, we want to render the homepage.handlebars template (the .handlebars extension is implied). 
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => {
+        // You did not need to serialize data before when you built API routes, because the res.json() method 
+        // automatically does that for you.
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        
+        // This will loop over and map each Sequelize object into a serialized version of itself, saving the 
+        // results in a new posts array.
+        res.render('homepage', { posts });
+
+        // pass a single post object into the homepage template (old line below)
+        // res.render('homepage', dbPostData[0].get({ plain: true }));
+
+        // non serialized object did not work (below)
+        //res.render('homepage', dbPostData[0]);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+const path = require('path'); // added for front end
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 // inporting the connection to sequilize
 const sequelize = require('./config/connection');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+// added for front end
+
+const exphbs = require('express-handlebars'); 
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// bring in front end
+app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on routes
 app.use(routes);
+
 
 // turn on connection to db and server
 // force:true, drops and re-creates all of the database tables and associations on startup.
